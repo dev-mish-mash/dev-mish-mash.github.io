889503747e97c2b3713758c56b45824b594bf16b
HoseungShin <nwdt3309@gmail.com>
2020-07-05 19:17:37 +0900

    [feat] WebDev 카테고리에서 content 보여주도록 구현

--- blog-v1/src/Components/CategoryComps/WebDev.jsx
+++ blog-v1/src/Components/CategoryComps/WebDev.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import ContentBox from '../PostingComps/ContentBox';
+import testImg from '../../img/testImg.png';
+
+class DevContents {
+    static items = [
+        {
+            imgSrc: testImg,
+            contentName: 'test',
+            uploadDate: '2020.07.04'
+        }
+    ]
+}
+
+const ContainerDiv = styled.div`
+    width: 1000px;
+    margin: 5rem auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: top;
+`;
 
 const WebDev = () => {
-    return <div />
+    return (
+        <ContainerDiv>
+            {DevContents.items.map((itemProps, index) => {
+                return <ContentBox { ...itemProps } key={index} />
+            })}
+        </ContainerDiv>
+    )
 }
 
 export default WebDev;
