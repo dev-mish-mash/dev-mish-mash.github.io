38509a9af70cc878cf6007d3078c357dbe235a60
HoseungShin <nwdt3309@gmail.com>
2020-07-05 19:17:06 +0900

    [bugfix] route 제대로 동작하지 않는 문제 수정

--- blog-v1/src/Components/RootPage.jsx
+++ blog-v1/src/Components/RootPage.jsx
@@ -4,14 +4,15 @@ import WebDev from './CategoryComps/WebDev';
 import Publications from './CategoryComps/Publications';
 import AboutMe from './CategoryComps/AboutMe';
 import Home from './Home';
+import styled from 'styled-components';
 
 const RootPage = () => {
     return (
         <Switch>
-            <Route path='/' component={Home} />
-            <Route path='/WebDev' component={WebDev} />
-            <Route path='/Publications' component={Publications} />
-            <Route path='/WebDev' component={AboutMe} />
+            <Route path='/' component={Home} exact />
+            <Route path='/WebDev' component={WebDev} exact />
+            <Route path='/Publications' component={Publications} exact />
+            <Route path='/AboutMe' component={AboutMe} exact />
             <Route render={({ location }) => (
                 <div>
                     <h2>이 페이지는 존재하지 않습니다:</h2>
